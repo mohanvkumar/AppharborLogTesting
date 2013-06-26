f5dc212347f2dedb4bc6dd9aa8941748dc71e9e2
Mohan Kumar V <mohan21_kumar@yahoo.co.in>
2013-06-26 15:22:47 +0530

    Logging Code For Testing in Appharbor

---
+++ AppharborLogTesting/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using log4net;
+
+namespace AppharborLogTesting
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(_Default));
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            logger.Debug("Debugging message");
+            logger.Info("Info message");
+            logger.Warn("Warning message");
+            logger.Error("Error message");
+            logger.Fatal("Fatal message");
+        }
+    }
+}
---
+++ AppharborLogTesting/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AppharborLogTesting.Global" Language="C#" %>
